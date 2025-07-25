574065a03fc382a158ce41aef806ce671993bd7a
Ninjamsc <slimclick@gmail.com>
2025-07-26 01:34:11 +0300

    branch master

---
+++ src/components/Categories.jsx
@@ -0,0 +1,18 @@
+function Categories() {
+  return (
+    <div class="categories">
+      <div class="container">
+        <ul>
+          <li class="active">Все</li>
+          <li>Мясные</li>
+          <li>Вегетарианские</li>
+          <li>Гриль</li>
+          <li>Острые</li>
+          <li>Закрытые</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default Categories;
---
+++ src/components/Header.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div class="header">
+      <div class="container">
+        <div class="header__logo">
+          <img width="38" src="img/pizza-logo.svg" alt="Pizza logo" />
+          <div>
+            <h1>Pizza MAX</h1>
+            <p>самая вкусная пицца во вселенной</p>
+          </div>
+        </div>
+        <div class="header__cart">
+          <img width="32" src="img/cart.svg" alt="Cart" />
+          <span>0 ₽</span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
