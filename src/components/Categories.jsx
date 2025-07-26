758839dbd3ac03d1443939583960807a6b955072
Ninjamsc <slimclick@gmail.com>
2025-07-26 22:00:01 +0000

    Jul 27, 2025, 1:00 AM

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -3,11 +3,20 @@ import React from "react";
 function Categories() {
   const [activeIndex, setActiveIndex] = React.useState(0);
 
+  const onClickCategory = (index) => {
+    setActiveIndex(index);
+  };
+
   return (
     <div className="categories">
       <div className="container">
         <ul>
-          <li className={activeIndex === 0 ? "active" : ""}>Все</li>
+          <li
+            onClick={onClickCategory}
+            className={activeIndex === 0 ? "active" : ""}
+          >
+            Все
+          </li>
           <li className={activeIndex === 1 ? "active" : ""}>Мясные</li>
           <li className={activeIndex === 2 ? "active" : ""}>Вегетарианские</li>
           <li className={activeIndex === 3 ? "active" : ""}>Гриль</li>
