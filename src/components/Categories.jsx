b3105ad9798823be68b23cccd2108abb0e39dc45
Ninjamsc <slimclick@gmail.com>
2025-07-26 21:58:27 +0000

    Jul 27, 2025, 12:58 AM

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,16 +1,18 @@
 import React from "react";
 
 function Categories() {
+  const [activeIndex, setActiveIndex] = React.useState(0);
+
   return (
     <div className="categories">
       <div className="container">
         <ul>
-          <li className="active">Все</li>
-          <li>Мясные</li>
-          <li>Вегетарианские</li>
-          <li>Гриль</li>
-          <li>Острые</li>
-          <li>Закрытые</li>
+          <li className={activeIndex === 0 ? "active" : ""}>Все</li>
+          <li className={activeIndex === 1 ? "active" : ""}>Мясные</li>
+          <li className={activeIndex === 2 ? "active" : ""}>Вегетарианские</li>
+          <li className={activeIndex === 3 ? "active" : ""}>Гриль</li>
+          <li className={activeIndex === 4 ? "active" : ""}>Острые</li>
+          <li className={activeIndex === 5 ? "active" : ""}>Закрытые</li>
         </ul>
       </div>
     </div>
