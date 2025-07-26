009330bd6c984fbf45f45b5ec8b504334cb240e3
Ninjamsc <slimclick@gmail.com>
2025-07-26 22:20:31 +0000

    Jul 27, 2025, 1:20 AM

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -20,42 +20,21 @@ function Categories() {
     <div className="categories">
       <div className="container">
         <ul>
-          <li
-            onClick={() => onClickCategory(0)}
-            className={activeIndex === 0 ? "active" : ""}
-          >
-            Все
-          </li>
-          <li
-            onClick={() => onClickCategory(1)}
-            className={activeIndex === 1 ? "active" : ""}
-          >
-            Мясные
-          </li>
-          <li
-            onClick={() => onClickCategory(2)}
-            className={activeIndex === 2 ? "active" : ""}
-          >
-            Вегетарианские
-          </li>
-          <li
-            onClick={() => onClickCategory(3)}
-            className={activeIndex === 3 ? "active" : ""}
-          >
-            Гриль
-          </li>
-          <li
-            onClick={() => onClickCategory(4)}
-            className={activeIndex === 4 ? "active" : ""}
-          >
-            Острые
-          </li>
-          <li
+          {categories.map((category, index) => (
+            <li
+              onClick={() => onClickCategory(index)}
+              className={activeIndex === index ? "active" : ""}
+              key={index}
+            >
+              {category}
+            </li>
+          ))}
+          {/* <li
             onClick={() => onClickCategory(5)}
             className={activeIndex === 5 ? "active" : ""}
           >
             Закрытые
-          </li>
+          </li> */}
         </ul>
       </div>
     </div>
