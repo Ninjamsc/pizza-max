e1c0507bfcd1764930bdbc50ad7a1fbe66b7a328
blagich <slimclick@gmail.com.com>
2025-10-01 21:50:01 +0300

    1121

--- src/App.js
+++ src/App.js
@@ -1,38 +1,38 @@
-import React from "react";
-import "./scss/app.scss";
-import Header from "./components/Header";
-import Home from "./pages/Home";
-import NotFound from "./pages/NotFound";
-import { Route, Routes } from "react-router-dom";
-import Cart from "./pages/Cart";
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Header from './components/Header'
+import Cart from './pages/Cart'
+import Home from './pages/Home'
+import NotFound from './pages/NotFound'
+import './scss/app.scss'
 
 // import pizzas from "./assets/pizzas.json";
 // import axios from "axios";
 
-export const SearchContext = React.createContext("");
+export const SearchContext = React.createContext('')
 
 function App() {
-  const [searchValue, setSearchValue] = React.useState("");
+	const [searchValue, setSearchValue] = React.useState('')
 
-  const path = window.location.pathname;
-  if (path === "/404") {
-    return <NotFound />;
-  }
+	const path = window.location.pathname
+	if (path === '/404') {
+		return <NotFound />
+	}
 
-  return (
-    <div className="wrapper">
-      <SearchContext.Provider value={{ searchValue, setSearchValue }}>
-        <Header />
-        <div className="content">
-          <Routes>
-            <Route path="" element={<Home searchValue={searchValue} />} />
-            <Route path="/cart" element={<Cart />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </div>
-      </SearchContext.Provider>
-    </div>
-  );
+	return (
+		<div className='wrapper'>
+			<SearchContext.Provider value={{ searchValue, setSearchValue }}>
+				<Header />
+				<div className='content'>
+					<Routes>
+						<Route path='' element={<Home searchValue={searchValue} />} />
+						<Route path='/cart' element={<Cart />} />
+						<Route path='*' element={<NotFound />} />
+					</Routes>
+				</div>
+			</SearchContext.Provider>
+		</div>
+	)
 }
 
-export default App;
+export default App
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,78 +1,80 @@
-import React from "react";
+import React from 'react'
 
-import Categories from "../components/Categories";
-import Sort from "../components/Sort";
-import PizzaBlock from "../components/PizzaBlock";
-import Skeleton from "../components/PizzaBlock/Skeleton";
-import Pagination from "../components/Pagination";
+import { SearchContext } from '../App'
+import Categories from '../components/Categories'
+import Pagination from '../components/Pagination'
+import PizzaBlock from '../components/PizzaBlock'
+import Skeleton from '../components/PizzaBlock/Skeleton'
+import Sort from '../components/Sort'
 
-const Home = ({ searchValue }) => {
-  const [items, setItems] = React.useState([]);
-  const [isLoading, setIsLoading] = React.useState(true);
-  const [categoryId, setCategoryId] = React.useState(0);
-  const [currentPage, setCurrentPage] = React.useState(1);
-  const [pageCount, setPageCount] = React.useState(1);
-  const [sortType, setSortType] = React.useState({
-    name: "популярности",
-    sortProperty: "rating",
-  });
+const Home = () => {
+	const { searchValue } = React.useContext(SearchContext)
+	const [items, setItems] = React.useState([])
+	const [isLoading, setIsLoading] = React.useState(true)
+	const [categoryId, setCategoryId] = React.useState(0)
+	const [currentPage, setCurrentPage] = React.useState(1)
+	const [pageCount, setPageCount] = React.useState(1)
+	const [sortType, setSortType] = React.useState({
+		name: 'популярности',
+		sortProperty: 'rating',
+	})
 
-  console.log(sortType);
+	console.log(sortType)
 
-  React.useEffect(() => {
-    setIsLoading(true);
+	React.useEffect(() => {
+		setIsLoading(true)
 
-    const sortBy = sortType.sortProperty;
-    // replace("-", "");
-    // const order = sortType.sortProperty.includes("-");
-    // ? "asc" : "desc";
-    const category = categoryId > 0 ? `&category=${categoryId}` : "";
-    const search = searchValue ? `&title_like=${searchValue}` : "";
+		const sortBy = sortType.sortProperty
+		// replace("-", "");
+		// const order = sortType.sortProperty.includes("-");
+		// ? "asc" : "desc";
+		const category = categoryId > 0 ? `&category=${categoryId}` : ''
+		const search = searchValue ? `&title_like=${searchValue}` : ''
 
-    fetch(
-      `http://localhost:3001/items?_page=${currentPage}&_per_page=8${category}&_sort=${sortBy}${search}`
-    )
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data.data);
-        console.log(data);
-        setItems(data.data);
-        setPageCount(data.pages);
-        setIsLoading(false);
-      });
-    window.scrollTo(0, 0);
-  }, [categoryId, sortType, searchValue, currentPage, pageCount]);
+		fetch(
+			`http://localhost:3001/items?_page=${currentPage}&_per_page=8${category}&_sort=${sortBy}${search}`
+		)
+			.then(res => res.json())
+			.then(data => {
+				console.log(data.data)
+				console.log(data)
+				setItems(data.data)
+				setPageCount(data.pages)
+				setIsLoading(false)
+			})
+		window.scrollTo(0, 0)
+	}, [categoryId, sortType, searchValue, currentPage, pageCount])
 
-  const pizzas = items
-    .filter((data) => {
-      if (data.title.toLowerCase().includes(searchValue.toLowerCase())) {
-        return true;
-      }
-      return false;
-    })
-    .map((data) => <PizzaBlock key={data.id} {...data} />);
+	const pizzas = items
+		.filter(data => {
+			if (data.title.toLowerCase().includes(searchValue.toLowerCase())) {
+				return true
+			}
+			return false
+		})
+		.map(data => <PizzaBlock key={data.id} {...data} />)
 
-  const skeletons = [...new Array(6)].map((_, index) => (
-    <Skeleton key={index} />
-  ));
+	const skeletons = [...new Array(6)].map((_, index) => (
+		<Skeleton key={index} />
+	))
 
-  return (
-    <div className="container">
-      <div className="content__top">
-        <Categories
-          value={categoryId}
-          onChangeCategory={(i) => setCategoryId(i)}
-        />
-        <Sort value={sortType} onChangeSort={(i) => setSortType(i)} />
-      </div>
-      <h2 className="content__title">Все пиццы</h2>
-      <div className="content__items">{isLoading ? skeletons : pizzas}</div>
-      <Pagination
-        onChangePage={(number) => setCurrentPage(number)}
-        pageCount={pageCount}
-      />
-    </div>
-  );
-};
+	return (
+		<div className='container'>
+			<div className='content__top'>
+				<Categories
+					value={categoryId}
+					onChangeCategory={i => setCategoryId(i)}
+				/>
+				<Sort value={sortType} onChangeSort={i => setSortType(i)} />
+			</div>
+			<h2 className='content__title'>Все пиццы</h2>
+			<div className='content__items'>{isLoading ? skeletons : pizzas}</div>
+			<Pagination
+				onChangePage={number => setCurrentPage(number)}
+				pageCount={pageCount}
+			/>
+		</div>
+	)
+}
 
-export default Home;
+export default Home
