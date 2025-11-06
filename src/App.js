bae074e26802dc6312f5da12ecc718ff4d3a139f
blagich <slimclick@gmail.com.com>
2025-11-06 23:33:16 +0300

    0611

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import Header from './components/Header'
-import Cart from './pages/Cart'
-import Home from './pages/Home'
-import NotFound from './pages/NotFound'
-import './scss/app.scss'
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import Cart from './pages/Cart';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
+import './scss/app.scss';
+import { decrement, increment } from './redux/slices/filterSlice';
+import { useDispatch, useSelector } from 'react-redux';
 
 // import pizzas from "./assets/pizzas.json";
 // import axios from "axios";
 
-export const SearchContext = React.createContext('')
+export const SearchContext = React.createContext('');
 
 function App() {
-	const [searchValue, setSearchValue] = React.useState('')
+	const [searchValue, setSearchValue] = React.useState('');
+	const count = useSelector(state => state.counter.value);
+	const dispatch = useDispatch();
 
-	const path = window.location.pathname
+	const path = window.location.pathname;
 	if (path === '/404') {
-		return <NotFound />
+		return <NotFound />;
 	}
 
 	return (
 		<div className='wrapper'>
+			<button
+				aria-label='Increment value'
+				onClick={() => dispatch(increment())}
+			>
+				Increment
+			</button>
+			<span>{count}</span>
+			<button
+				aria-label='Decrement value'
+				onClick={() => dispatch(decrement())}
+			>
+				Decrement
+			</button>
 			<SearchContext.Provider value={{ searchValue, setSearchValue }}>
 				<Header />
 				<div className='content'>
@@ -32,7 +49,7 @@ function App() {
 				</div>
 			</SearchContext.Provider>
 		</div>
-	)
+	);
 }
 
-export default App
+export default App;
